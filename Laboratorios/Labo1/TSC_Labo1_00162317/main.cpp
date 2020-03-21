3d88b4c8ac50f4da4a33b491558dc28ede2379a0
Roberto Hernandez <hernandez.roberto014@gmail.com>
2020-03-21 00:00:35 -0600

    Ups !

--- Laboratorios/Labo1/TSC_Labo1_00162317/main.cpp
+++ Laboratorios/Labo1/TSC_Labo1_00162317/main.cpp
@@ -7,17 +7,17 @@ int main() {
     Matrix MatrixA, MatrixB;
     zeroes(MatrixA, 3);
     //////////////////////////////////////
-    MatrixA.at(0).at(0) = 5;
-    MatrixA.at(0).at(1) = 8;
-    MatrixA.at(0).at(2) = 2;
+    MatrixA.at(0).at(0) = 2;
+    MatrixA.at(0).at(1) = 2;
+    MatrixA.at(0).at(2) = 3;
     //////////////////////////////////////
-    MatrixA.at(1).at(0) = 9;
-    MatrixA.at(1).at(1) = 1;
-    MatrixA.at(1).at(2) = 7;
+    MatrixA.at(1).at(0) = 4;
+    MatrixA.at(1).at(1) = 5;
+    MatrixA.at(1).at(2) = 6;
     //////////////////////////////////////
-    MatrixA.at(2).at(0) = 2;
-    MatrixA.at(2).at(1) = 6;
-    MatrixA.at(2).at(2) = 1;
+    MatrixA.at(2).at(0) = 7;
+    MatrixA.at(2).at(1) = 8;
+    MatrixA.at(2).at(2) = 9;
     //////////////////////////////////////
     showMatrix(MatrixInversa(MatrixA, MatrixB));
 
