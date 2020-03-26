b8ef49cbf19d5e08aa9fdec56b3c52e9af370839
Roberto Hernandez <hernandez.roberto014@gmail.com>
2020-03-26 17:19:10 -0600

    Se adapto para poder empezar a trabajar !!

---
+++ Laboratorios/Labo2/TSC_LABO2_00162317/cmake-build-debug/CMakeFiles/TSC_LABO2_00162317.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TSC_LABO2_00162317.dir/main.cpp.obj"
+  "TSC_LABO2_00162317.exe"
+  "TSC_LABO2_00162317.exe.manifest"
+  "TSC_LABO2_00162317.pdb"
+  "libTSC_LABO2_00162317.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TSC_LABO2_00162317.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Laboratorios/Labo2/TSC_LABO2_00162317/cmake-build-debug/CMakeFiles/TSC_LABO2_00162317.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for TSC_LABO2_00162317.
+# This may be replaced when dependencies are built.
