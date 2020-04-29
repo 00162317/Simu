1234c8c8496ce3aad7001dd1950aca6f83ce8de0
Roberto Hernandez <hernandez.roberto014@gmail.com>
2020-04-29 13:37:26 -0600

    Creando el proyecto

---
+++ Laboratorios/Labo-3/TSC_TAREA3_00162317/cmake-build-debug/CMakeFiles/TSC_TAREA3_00162317.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TSC_TAREA3_00162317.dir/main.cpp.obj"
+  "TSC_TAREA3_00162317.exe"
+  "TSC_TAREA3_00162317.exe.manifest"
+  "TSC_TAREA3_00162317.pdb"
+  "libTSC_TAREA3_00162317.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TSC_TAREA3_00162317.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
